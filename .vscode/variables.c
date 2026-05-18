3382d470ae3fe177e362f3992e5993e35159c575
Jose Yepez <yejo32602@gmail.com>
2026-05-18 20:17:46 +0000

    Initial Commit

---
+++ .vscode/variables.c
@@ -0,0 +1,16 @@
+#include <stdio.h> 
+
+// Recall that in python you could do the following
+// number = 65
+// phrase = "Exploding Hearts"
+
+// BUT IN C!!
+// When declaring a file you need to explicitly state what the variable is.
+
+int main(void){
+    int my_number = 32;
+
+    printf("Your number is: %d \n",my_number);
+    return 0;
+}
+
