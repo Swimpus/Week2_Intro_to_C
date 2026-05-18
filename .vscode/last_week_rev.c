c4a7d62de742a05e9ffd7f27d034578edb939814
Jose Yepez <yejo32602@gmail.com>
2026-05-18 20:06:10 +0000

    Initial Commit

---
+++ .vscode/last_week_rev.c
@@ -0,0 +1,8 @@
+# include <stdio.h> // Necessary for standard input ouput!!
+
+int main(void){ // Program entry point AKA a function start.
+    printf("Night and day blending\n");
+
+    return 0; // Every function needs to return something
+              // We return 0 as common etiquette so signify a sucessful execution
+}
