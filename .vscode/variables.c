2c1a186f6ee053e39d8f78278382c5ee3c24ec1b
Jose Yepez <yejo32602@gmail.com>
2026-05-19 03:04:22 +0000

    Added notes on variables + string

--- .vscode/variables.c
+++ .vscode/variables.c
@@ -8,9 +8,27 @@
 // When declaring a file you need to explicitly state what the variable is.
 
 int main(void){
-    int my_number = 32;
+    int my_number = 32; // Declare what your variable type is + its name, then it's contents
 
     printf("Your number is: %d \n",my_number);
+
+    char my_symbol = '@';
+
+    printf("Your character is: %c \n",my_symbol);
+
+
+    // For strings:
+
+    // Method One
+    // my_line is a pointer that points to a string literal
+    char *my_line = "In the name of Justice!!";
+    printf("Your string is: %s \n",my_line);
+
+    // Method Two
+    // Character array AKA List of Characters
+    char my_line2[]= "It's a mechanical bull, the number one!";
+    printf("String number two is: %s \n",my_line2);
+
     return 0;
 }
 
